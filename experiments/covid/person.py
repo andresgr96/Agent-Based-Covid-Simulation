5a0118c37b0e9e8faedd4c05799bbdfd9fdece45
andres <andresgarciar96@gmail.com>
2021-06-13 18:19:05 +0200

    main codebase started

--- experiments/covid/person.py
+++ experiments/covid/person.py
@@ -8,4 +8,23 @@ from simulation.utils import *
 
 class Person(Agent):
     """ """
-    pass
+    def __init__(
+            self, pos, v, person, index: int, image: str = "experiments/covid/images/neutral-face_1f610.png"
+
+    ) -> None:
+        super(Person, self).__init__(
+            pos,
+            v,
+            image,
+            max_speed=config["agent"]["max_speed"],
+            min_speed=config["agent"]["min_speed"],
+            mass=config["agent"]["mass"],
+            width=config["agent"]["width"],
+            height=config["agent"]["height"],
+            dT=config["agent"]["dt"],
+            index=index,
+        )
+
+    def update_actions(self) -> None:
+
+        pass
