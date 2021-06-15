644fa172b020dd9d311e69697d7f5376b470bfeb
andres <andresgarciar96@gmail.com>
2021-06-15 22:37:57 +0200

    behavioral state machine impleemnted, super stage bettered

---
+++ experiments/covid/pacient.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pygame
+
+from experiments.covid import population
+from experiments.covid.config import config
+from simulation.agent import Agent
+from simulation.swarm import Swarm
+from simulation.utils import *
+import experiments.covid.population
+
+experiment = "super"
+class Pacient(Agent):
+    """ """
+
+    def __init__(
+            self, pos, v, pacient, index: int, susceptible, infectious, recovered, image: str = "experiments/covid/images/inf.png", countInf = 0,countState = 0,sus = pygame.image.load("experiments/covid/images/sus.png"),
+        inf = pygame.image.load("experiments/covid/images/inf.png"),
+        rec = pygame.image.load("experiments/covid/images/cured.png"),
+        wandering = False,
+        still = True,
+        leaving = False,
+        joining = False
+
+    ) -> None:
+        super(Pacient, self).__init__(
+            pos,
+            v,
+            image,
+            max_speed=config["agent"]["max_speed"],
+            min_speed=config["agent"]["min_speed"],
+            mass=config["agent"]["mass"],
+            width=config["agent"]["width"],
+            height=config["agent"]["height"],
+            dT=0,
+            index=index,
+        )
+        self.pacient = pacient
+        self.countInf = countInf
+        self.countState = countState
+        self.susceptible = susceptible
+        self.infectious = infectious
+        self.recovered = recovered
+        self.sus = sus
+        self.inf = inf
+        self.rec = rec
+        self.wandering = wandering
+        self.leaving = leaving
+        self.joining = joining
+        self.still = still
+
+    def update_actions(self) -> None:
+        if self.infectious == True:
+            self.pacient.datapoints.append("I")
--- experiments/covid/population.py
+++ experiments/covid/population.py
@@ -1,10 +1,11 @@
 from experiments.covid.config import config
+from experiments.covid.pacient import Pacient
 from experiments.covid.person import Person
 from simulation.swarm import Swarm
 from simulation.utils import *
 import random
 
-experiment = "base"
+experiment = "super"
 
 class Population(Swarm):
     """Class that represents the Population for the Covid experiment. TODO"""
@@ -48,97 +49,92 @@ class Population(Swarm):
         elif experiment == "super":
         # Horizontal row upper
             self.objects.add_object(file="experiments/covid/images/square_-_black_simple.svg.png", pos=[100,100], scale=[200, 200],
-                                    obj_type="obstacle")
+                                    obj_type="site")
 
             self.objects.add_object(file="experiments/covid/images/square_-_black_simple.svg.png", pos=[300, 100], scale=[200, 200],
-                                    obj_type="obstacle")
+                                    obj_type="site")
 
             self.objects.add_object(file="experiments/covid/images/square_-_black_simple.svg.png", pos=[500, 100], scale=[200, 200],
-                                    obj_type="obstacle")
+                                    obj_type="site")
 
             self.objects.add_object(file="experiments/covid/images/square_-_black_simple.svg.png", pos=[700, 100], scale=[200, 200],
-                                    obj_type="obstacle")
+                                    obj_type="site")
 
             self.objects.add_object(file="experiments/covid/images/square_-_black_simple.svg.png", pos=[900, 100], scale=[200, 200],
-                                    obj_type="obstacle")
+                                    obj_type="site")
 
             # Vertical row right
             self.objects.add_object(file="experiments/covid/images/square_-_black_simple.svg.png", pos=[900, 300], scale=[200, 200],
-                                    obj_type="obstacle")
+                                    obj_type="site")
 
             self.objects.add_object(file="experiments/covid/images/square_-_black_simple.svg.png", pos=[900, 500], scale=[200, 200],
-                                    obj_type="obstacle")
+                                    obj_type="site")
 
             self.objects.add_object(file="experiments/covid/images/square_-_black_simple.svg.png", pos=[900, 700], scale=[200, 200],
-                                    obj_type="obstacle")
+                                    obj_type="site")
 
             self.objects.add_object(file="experiments/covid/images/square_-_black_simple.svg.png", pos=[900, 900], scale=[200, 200],
-                                    obj_type="obstacle")
+                                    obj_type="site")
 
             # Horizontal row lower
             self.objects.add_object(file="experiments/covid/images/square_-_black_simple.svg.png", pos=[100, 900],
                                     scale=[200, 200],
-                                    obj_type="obstacle")
+                                    obj_type="site")
 
             self.objects.add_object(file="experiments/covid/images/square_-_black_simple.svg.png", pos=[300, 900],
                                     scale=[200, 200],
-                                    obj_type="obstacle")
+                                    obj_type="site")
 
             self.objects.add_object(file="experiments/covid/images/square_-_black_simple.svg.png", pos=[500, 900],
                                     scale=[200, 200],
-                                    obj_type="obstacle")
+                                    obj_type="site")
 
             self.objects.add_object(file="experiments/covid/images/square_-_black_simple.svg.png", pos=[700, 900],
                                     scale=[200, 200],
-                                    obj_type="obstacle")
+                                    obj_type="site")
 
             # Vertical row left
             self.objects.add_object(file="experiments/covid/images/square_-_black_simple.svg.png", pos=[100, 300],
                                     scale=[200, 200],
-                                    obj_type="obstacle")
+                                    obj_type="site")
 
             self.objects.add_object(file="experiments/covid/images/square_-_black_simple.svg.png", pos=[100, 500],
                                     scale=[200, 200],
-                                    obj_type="obstacle")
+                                    obj_type="site")
 
             self.objects.add_object(file="experiments/covid/images/square_-_black_simple.svg.png", pos=[100, 700],
                                     scale=[200, 200],
-                                    obj_type="obstacle")
+                                    obj_type="site")
 
             # Supermarket
             self.objects.add_object(file="experiments/covid/images/square_-_black_simple.svg.png", pos=[500,500],
                                     scale=[350, 350],
-                                    obj_type="obstacle")
+                                    obj_type="site")
+
+            self.add_agent(
+            Pacient(pos=(500,500), v=None, pacient=self, index=41, infectious=True, susceptible=False,
+                   recovered=False))
 
             '''min_x, max_x = area(0, 4000)
             min_y, max_y = area(0, 4000)'''
             rooms = [[100.0, 700.0],[100.0, 500.0],[100.0, 300.0],[700.0, 900.0],[500.0, 900.0],[300.0, 900.0],[100.0, 900.0],[900.0, 900.0],[900.0, 700.0],[900.0, 500.0],[900.0, 300.0],
                      [900.0, 100.0],[700.0, 100.0],[500.0, 100.0],[300.0, 100.0],[100.0,100.0]]
             # add agents to the environment
-            for index, agent in enumerate(range(num_infected)):
-                '''coordinates = generate_coordinates(self.screen)
-                while (
-                        coordinates[0] >= max_x
-                        or coordinates[0] <= min_x
-                        or coordinates[1] >= max_y
-                        or coordinates[1] <= min_y
-                ):
-                    coordinates = generate_coordinates(self.screen)'''
-                random_room = random.choice(rooms)
-                random_room[0] += np.random.uniform(0, 10)
-                random_room[1] += np.random.uniform(0, 10)
-                self.add_agent(Person(pos=np.array(random_room), v=None, person=self, index=index, susceptible=False, infectious=True,recovered=False))
+            # for index, agent in enumerate(range(num_infected)):
+            #     '''coordinates = generate_coordinates(self.screen)
+            #     while (
+            #             coordinates[0] >= max_x
+            #             or coordinates[0] <= min_x
+            #             or coordinates[1] >= max_y
+            #             or coordinates[1] <= min_y
+            #     ):
+            #         coordinates = generate_coordinates(self.screen)'''
+            #     random_room = random.choice(rooms)
+            #     random_room[0] += np.random.uniform(0, 10)
+            #     random_room[1] += np.random.uniform(0, 10)
+            #     self.add_agent(Person(pos=np.array(random_room), v=None, person=self, index=index, susceptible=False, infectious=True,recovered=False))
 
             for index, agent in enumerate(range(num_agents)):
-                '''coordinates = generate_coordinates(self.screen)
-                while (
-                        coordinates[0] >= max_x
-                        or coordinates[0] <= min_x
-                        or coordinates[1] >= max_y
-                        or coordinates[1] <= min_y
-                ):
-                    coordinates = generate_coordinates(self.screen)
-                print(coordinates)'''
                 random_room = random.choice(rooms)
                 random_room[0] += np.random.uniform(0,10)
                 random_room[1] += np.random.uniform(0,10)
