8f25a383f255a77a9042cfed76df15ba69c45986
Barnflair2 <77009100+Barnflair2@users.noreply.github.com>
2021-06-16 17:12:02 +0200

    Update population.py

--- experiments/covid/population.py
+++ experiments/covid/population.py
@@ -2,6 +2,7 @@ from experiments.covid.config import config
 from experiments.covid.person import Person
 from simulation.swarm import Swarm
 from simulation.utils import *
+import random
 
 
 class Population(Swarm):
@@ -9,30 +10,97 @@ class Population(Swarm):
 
     def __init__(self, screen_size) -> None:
         super(Population, self).__init__(screen_size)
-        # To do
 
-    def initialize(self, num_agents: int) -> None:
+
+    def initialize(self, num_agents: int, num_infected: int) -> None:
         """
         Args:
             num_agents (int):
-
         """
+        # Wine corner
+        self.objects.add_object(file="experiments/covid/images/88226-200.png", pos=[100, 150], scale=[200, 100],
+                                obj_type="obstacle")
 
-        # To Do
-        # code snipet (not complete) to avoid initializing agents on obstacles
-        # given some coordinates and obstacles in the environment, this repositions the agent
-        coordinates = generate_coordinates(self.screen)
+        self.objects.add_object(file="experiments/covid/images/88226-200.png", pos=[100, 350], scale=[200, 100],
+                                obj_type="obstacle")
 
-        if config["population"]["obstacles"]:  # you need to define this variable
-            for obj in self.objects.obstacles:
-                rel_coordinate = relative(
-                    coordinates, (obj.rect[0], obj.rect[1])
-                )
-                try:
-                    while obj.mask.get_at(rel_coordinate):
-                        coordinates = generate_coordinates(self.screen)
-                        rel_coordinate = relative(
-                            coordinates, (obj.rect[0], obj.rect[1])
-                        )
-                except IndexError:
-                    pass
+        # Left Corner
+        self.objects.add_object(file="experiments/covid/images/88226-200.png", pos=[250, 250], scale=[100, 300],
+                                obj_type="obstacle")
+
+        self.objects.add_object(file="experiments/covid/images/88226-200.png", pos=[400, 250], scale=[100, 300],
+                                obj_type="obstacle")
+
+        self.objects.add_object(file="experiments/covid/images/88226-200.png", pos=[550, 250], scale=[100, 300],
+                                obj_type="obstacle")
+
+        self.objects.add_object(file="experiments/covid/images/88226-200.png", pos=[700, 250], scale=[100, 300],
+                                obj_type="obstacle")
+
+        # Cheese and stuff
+        self.objects.add_object(file="experiments/covid/images/88226-200.png", pos=[900, 150], scale=[200, 100],
+                                obj_type="obstacle")
+
+        self.objects.add_object(file="experiments/covid/images/88226-200.png", pos=[900, 350], scale=[200, 100],
+                                obj_type="obstacle")
+
+        # Middle path thingy
+        self.objects.add_object(file="experiments/covid/images/88226-200.png", pos=[350, 500], scale=[1000, 50],
+                                obj_type="obstacle")
+
+        # Lower and cash-out
+        self.objects.add_object(file="experiments/covid/images/88226-200.png", pos=[150, 700], scale=[100, 200],
+                                obj_type="obstacle")
+
+        self.objects.add_object(file="experiments/covid/images/88226-200.png", pos=[250, 700], scale=[100, 200],
+                                obj_type="obstacle")
+
+        self.objects.add_object(file="experiments/covid/images/88226-200.png", pos=[350, 700], scale=[100, 200],
+                                obj_type="obstacle")
+
+        self.objects.add_object(file="experiments/covid/images/88226-200.png", pos=[450, 700], scale=[100, 200],
+                                obj_type="obstacle")
+
+        self.objects.add_object(file="experiments/covid/images/88226-200.png", pos=[550, 900], scale=[100, 650],
+                                obj_type="obstacle")
+
+        # Fruit and Vegi part
+        self.objects.add_object(file="experiments/covid/images/88226-200.png", pos=[700, 750], scale=[100, 300],
+                                obj_type="obstacle")
+
+        self.objects.add_object(file="experiments/covid/images/88226-200.png", pos=[850, 750], scale=[100, 300],
+                                obj_type="obstacle")
+
+        # Supermarket
+        self.objects.add_object(file="experiments/covid/images/square_-_black_simple.svg.png", pos=[500,500],
+                                scale=[1655, 1655],
+                                obj_type="obstacle")
+
+        min_x, max_x = area(100, 3500)
+        min_y, max_y = area(100, 3500)
+        rooms = [[100, 700],[100, 500],[100, 300],[700, 900],[500, 900],[300, 900],[100, 900],[900, 900],[900, 700],[900, 500],[900, 300],
+                 [900, 100],[700, 100],[500, 100],[300, 100],[100,100]]
+        # add agents to the environment
+        for index, agent in enumerate(range(num_infected)):
+            coordinates = generate_coordinates(self.screen)
+            while (
+                    coordinates[0] >= max_x
+                    or coordinates[0] <= min_x
+                    or coordinates[1] >= max_y
+                    or coordinates[1] <= min_y
+            ):
+                coordinates = generate_coordinates(self.screen)
+            self.add_agent(Person(pos=np.array(coordinates), v=None, person=self, index=index, susceptible=False, infectious=True,recovered=False))
+
+        for index, agent in enumerate(range(num_agents)):
+            coordinates = generate_coordinates(self.screen)
+            while (
+                    coordinates[0] >= max_x
+                    or coordinates[0] <= min_x
+                    or coordinates[1] >= max_y
+                    or coordinates[1] <= min_y
+            ):
+                coordinates = generate_coordinates(self.screen)
+            print(coordinates)
+            self.add_agent(Person(pos=np.array(coordinates), v=None, person=self, index=index, infectious = False,susceptible=True,recovered=False))
+
